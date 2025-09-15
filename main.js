dd9c02b3f87e13e66fde17c04861a2fdfdfb07d1
Khinotska Sophia bc2025-2 <Sofiia.Khinotska@lnu.edu.ua>
2025-09-15 23:04:54 +0300

    Add a program that will construct a URL

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+const DATE = "202202";  
+const { URL, URLSearchParams } = require("url");
+const baseUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/res");
+baseUrl.search = new URLSearchParams({
+  date: DATE,
+  json: ""   
+});
+console.log("Сконструйований URL:", baseUrl.toString());
